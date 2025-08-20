8a42af4ca12929e01f9144607bf0d4b20683f94f
unknown <frachris91@gmail.com>
2025-08-20 14:59:44 +0200

    cleaned up unused code

--- Backend/API/API/Program.cs
+++ Backend/API/API/Program.cs
@@ -1,6 +1,6 @@
 using API.Model;
+using API.Services;
 using API.Services.Interfaces;
-using API.Services.ItemServices;
 using API.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
---
+++ Backend/API/API/Services/EndPointService.cs
@@ -0,0 +1,30 @@
+﻿using API.Services.Interfaces;
+
+namespace API.Services;
+
+public class EndpointMapperService : IEndpointMapper
+{
+    public void MapEndpoints<T>(WebApplication app, string route)
+    {
+        app.MapGet($"/{route}", (ICrudService<T> storage) =>
+        {
+            return storage.GetAll();
+        });
+
+        // app.MapPost($"/{route}", (ICrudService<T> storage) =>
+        // {
+        //
+        // });
+        //
+        // app.MapPut($"/{route}/{{id}}", (int id, ICrudService<T> storage) =>
+        // {
+        //
+        // });
+
+        app.MapDelete($"/{route}/{{id}}", (int id, ICrudService<T> storage) =>
+        {
+            storage.Delete(id);
+            return Results.NoContent();
+        });
+    }
+}
